71818e868794ae5ddbc518dc57a6312c004d83f2
Jaggler3 <martin.protostar@gmail.com>
2017-06-30 14:56:50 -0400

    File exists check

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ for(let i = 0; i < routeLines.length; i++)
 	let spl = routeLines[i].split(" ");
 	app.get('/' + spl[0].trim(), function(req, res)
 	{
-		fs.readFile('./public/' + spl[1].trim(), 'utf8', function (err, data)
+		if(fs.existsSync('./public/' + spl[1].trim()))
 		{
-			if (err) { throw err; }
-			res.send(data.toString());
-		});
+			fs.readFile('./public/' + spl[1].trim(), 'utf8', function (err, data)
+			{
+				if (err) { throw err; }
+				res.send(data.toString());
+			});
+		}
 	});
 }
 
